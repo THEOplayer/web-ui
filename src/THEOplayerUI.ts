3f2d6d50aec31e241e7bea88dbd3eba06ea7dd13
Mattias Buelens <mattias.buelens@theoplayer.com>
2022-11-14 11:11:55 +0100

    Set up TypeScript

---
+++ src/THEOplayerUI.ts
@@ -0,0 +1,55 @@
+const template = document.createElement('template');
+// language=HTML
+template.innerHTML = `
+    <style>
+        :host {
+            box-sizing: border-box;
+            position: relative;
+            width: 600px;
+            height: 400px;
+            display: inline-block;
+            line-height: 0;
+            background-color: var(--media-background-color, #000);
+        }
+
+        .layer:not(.media-layer) {
+            position: absolute;
+            top: 0;
+            left: 0;
+            bottom: 0;
+            right: 0;
+            display: flex;
+            flex-flow: column nowrap;
+            align-items: start;
+            pointer-events: none;
+            background: none;
+        }
+
+        .middle.chrome {
+            display: inline;
+            flex-grow: 1;
+            pointer-events: none;
+            background: none;
+        }
+    </style>
+
+    <slot name="media" class="layer media-layer"></slot>
+    <slot name="poster" class="layer poster-layer"></slot>
+    <div class="layer vertical-layer">
+        <slot name="top-chrome" class="top chrome"></slot>
+        <slot name="middle-chrome" class="middle chrome"></slot>
+        <slot name="centered-chrome" class="layer centered-layer center centered chrome"></slot>
+    ${/* default, effectively "bottom-chrome" */ ''}
+        <slot class="bottom chrome"></slot>
+    </div>
+`;
+
+class THEOplayerUI extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot!.appendChild(template.content.cloneNode(true));
+    }
+}
+
+customElements.define('theoplayer-ui', THEOplayerUI);
