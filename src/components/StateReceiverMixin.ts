1cea0c4f8af429711f6855b8e13292f1f359e43d
Mattias Buelens <mattias.buelens@theoplayer.com>
2023-02-08 18:12:40 +0100

    Document StateReceiverMixin

--- src/components/StateReceiverMixin.ts
+++ src/components/StateReceiverMixin.ts
@@ -2,6 +2,7 @@ import { Constructor, fromArrayLike, isArray } from '../util/CommonUtils';
 import type { ChromelessPlayer, THEOplayerError, VideoQuality } from 'theoplayer';
 import type { StreamType } from '../util/StreamType';
 
+/** @internal */
 export const StateReceiverProps = 'theoplayerUiObservedProperties' as const;
 
 export interface StateReceiverPropertyMap {
@@ -19,14 +20,39 @@ export type StateReceiverMethods = {
     [T in keyof StateReceiverPropertyMap as `set${Capitalize<T>}`]?: (value: StateReceiverPropertyMap[T]) => void;
 };
 
+/**
+ * A custom element that automatically receives selected state updates
+ * from an ancestor [`<theoplayer-ui>`]{@link UIContainer} element.
+ *
+ * Do not implement this interface directly, instead use {@link StateReceiverMixin}.
+ *
+ * @see {@link StateReceiverMixin}
+ */
 export interface StateReceiverElement extends StateReceiverMethods, Element {
+    /**
+     * The names of the properties this element will receive.
+     */
     readonly [StateReceiverProps]: Array<keyof StateReceiverPropertyMap>;
 }
 
+/** @internal */
 export function isStateReceiverElement(element: Element): element is StateReceiverElement {
     return StateReceiverProps in element && isArray((element as Partial<StateReceiverElement>)[StateReceiverProps]);
 }
 
+/**
+ * A [mixin class](https://www.typescriptlang.org/docs/handbook/mixins.html) to apply on the superclass of a custom element,
+ * such that it will automatically receive selected state updates from an ancestor [`<theoplayer-ui>`]{@link UIContainer} element.
+ *
+ * For each property name in `props`, the custom element *MUST* implement a corresponding setter method.
+ * For example, if `props` equals `["player", "fullscreen"]`, then the element must have a `setPlayer(player)`
+ * and `setFullscreen(fullscreen)` method.
+ *
+ * @param base - The superclass for the new element class.
+ * @param props - The names of the properties this element will receive.
+ * @returns A class constructor that extends `base` and implements {@link StateReceiverElement}.
+ * @see {@link StateReceiverElement}
+ */
 export function StateReceiverMixin<T extends Constructor<Element>>(base: T, props: Array<keyof StateReceiverPropertyMap>) {
     abstract class StateReceiver extends base implements StateReceiverElement {
         get [StateReceiverProps](): Array<keyof StateReceiverPropertyMap> {
@@ -37,6 +63,7 @@ export function StateReceiverMixin<T extends Constructor<Element>>(base: T, prop
     return StateReceiver;
 }
 
+/** @internal */
 export async function forEachStateReceiverElement(
     element: Element,
     playerElement: HTMLElement,
