435a98bd45210e2b2dc777d80b5b314bb536e36f
Mattias Buelens <mattias.buelens@theoplayer.com>
2023-02-16 11:16:28 +0100

    Fix indentation

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -13,99 +13,99 @@ const { browserslist } = JSON.parse(await readFile('./package.json', { encoding:
 const production = process.env.BUILD === 'production';
 
 export default defineConfig([
-{
-    input: './src/index.ts',
-    output: [
-        {
-            file: './dist/THEOplayerUI.js',
-            format: 'umd',
-            name: 'THEOplayerUI',
-            sourcemap: true,
-            indent: false,
-            globals: {
-                theoplayer: 'THEOplayer'
-            }
-        },
-        {
-            file: './dist/THEOplayerUI.mjs',
-            format: 'es',
-            sourcemap: true,
-            indent: false
-        }
-    ],
-    context: 'self',
-    external: ['theoplayer'],
-    plugins: [
-        // Use TypeScript's module resolution for source files, and Node's for dependencies.
-        typescriptPaths(),
-        nodeResolve(),
-        // Run PostCSS on .css files.
-        postcss({
-            include: './src/**/*.css',
-            inject: false,
-            plugins: [
-                postcssPresetEnv({
-                    browsers: browserslist,
-                    autoprefixer: { grid: 'no-autoplace' },
-                    enableClientSidePolyfills: false
-                }),
-                postcssMixins()
-            ],
-            minimize: production
-        }),
-        // Minify HTML and CSS.
-        minifyHTML({
-            include: ['./src/**/*.html'],
-            removeComments: production,
-            removeRedundantAttributes: true,
-            sortClassName: true,
-            collapseWhitespace: production
-        }),
-        // Import HTML and SVG as strings.
-        string({
-            include: ['./src/**/*.html', './src/**/*.svg']
-        }),
-        // Transpile TypeScript.
-        swc({
-            include: './src/**',
-            sourceMaps: true,
-            tsconfig: false,
-            env: {
-                targets: browserslist
-            },
-            jsc: {
-                loose: true,
-                externalHelpers: true,
-                parser: {
-                    syntax: 'typescript'
+    {
+        input: './src/index.ts',
+        output: [
+            {
+                file: './dist/THEOplayerUI.js',
+                format: 'umd',
+                name: 'THEOplayerUI',
+                sourcemap: true,
+                indent: false,
+                globals: {
+                    theoplayer: 'THEOplayer'
                 }
-            }
-        }),
-        // Transpile dependencies for older browsers.
-        swc({
-            include: './node_modules/**',
-            exclude: './src/**',
-            sourceMaps: true,
-            tsconfig: false,
-            env: {
-                targets: browserslist
             },
-            jsc: {
-                loose: true,
-                externalHelpers: true
+            {
+                file: './dist/THEOplayerUI.mjs',
+                format: 'es',
+                sourcemap: true,
+                indent: false
             }
-        }),
-        ...(production
-            ? [
-                  minify({
-                      sourceMap: true,
-                      mangle: {
+        ],
+        context: 'self',
+        external: ['theoplayer'],
+        plugins: [
+            // Use TypeScript's module resolution for source files, and Node's for dependencies.
+            typescriptPaths(),
+            nodeResolve(),
+            // Run PostCSS on .css files.
+            postcss({
+                include: './src/**/*.css',
+                inject: false,
+                plugins: [
+                    postcssPresetEnv({
+                        browsers: browserslist,
+                        autoprefixer: { grid: 'no-autoplace' },
+                        enableClientSidePolyfills: false
+                    }),
+                    postcssMixins()
+                ],
+                minimize: production
+            }),
+            // Minify HTML and CSS.
+            minifyHTML({
+                include: ['./src/**/*.html'],
+                removeComments: production,
+                removeRedundantAttributes: true,
+                sortClassName: true,
+                collapseWhitespace: production
+            }),
+            // Import HTML and SVG as strings.
+            string({
+                include: ['./src/**/*.html', './src/**/*.svg']
+            }),
+            // Transpile TypeScript.
+            swc({
+                include: './src/**',
+                sourceMaps: true,
+                tsconfig: false,
+                env: {
+                    targets: browserslist
+                },
+                jsc: {
+                    loose: true,
+                    externalHelpers: true,
+                    parser: {
+                        syntax: 'typescript'
+                    }
+                }
+            }),
+            // Transpile dependencies for older browsers.
+            swc({
+                include: './node_modules/**',
+                exclude: './src/**',
+                sourceMaps: true,
+                tsconfig: false,
+                env: {
+                    targets: browserslist
+                },
+                jsc: {
+                    loose: true,
+                    externalHelpers: true
+                }
+            }),
+            ...(production
+                ? [
+                      minify({
+                          sourceMap: true,
+                          mangle: {
+                              toplevel: true
+                          },
                           toplevel: true
-                      },
-                      toplevel: true
-                  })
-              ]
-            : [])
-    ]
-}
+                      })
+                  ]
+                : [])
+        ]
+    }
 ]);
