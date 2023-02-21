8de20d1aa3126cf6a025a825956faf1d0553ebbd
Mattias Buelens <649348+MattiasBuelens@users.noreply.github.com>
2023-02-21 11:38:49 +0100

    Add type definitions to build (#5)

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -8,102 +8,128 @@ import postcssPresetEnv from 'postcss-preset-env';
 import postcssMixins from 'postcss-mixins';
 import { readFile } from 'fs/promises';
 import { string } from 'rollup-plugin-string';
+import dts from 'rollup-plugin-dts';
 
-const { browserslist } = JSON.parse(await readFile('./package.json', { encoding: 'utf8' }));
+const fileName = 'THEOplayerUI';
+const umdName = 'THEOplayerUI';
+
+const { browserslist, version, license } = JSON.parse(await readFile('./package.json', { encoding: 'utf8' }));
 const production = process.env.BUILD === 'production';
 
-export default defineConfig({
-    input: './src/index.ts',
-    output: [
-        {
-            file: './dist/THEOplayerUI.js',
-            format: 'umd',
-            name: 'THEOplayerUI',
-            sourcemap: true,
-            indent: false,
-            globals: {
-                theoplayer: 'THEOplayer'
-            }
-        },
-        {
-            file: './dist/THEOplayerUI.mjs',
-            format: 'es',
-            sourcemap: true,
-            indent: false
-        }
-    ],
-    context: 'self',
-    external: ['theoplayer'],
-    plugins: [
-        // Use TypeScript's module resolution for source files, and Node's for dependencies.
-        typescriptPaths(),
-        nodeResolve(),
-        // Run PostCSS on .css files.
-        postcss({
-            include: './src/**/*.css',
-            inject: false,
-            plugins: [
-                postcssPresetEnv({
-                    browsers: browserslist,
-                    autoprefixer: { grid: 'no-autoplace' },
-                    enableClientSidePolyfills: false
-                }),
-                postcssMixins()
-            ],
-            minimize: production
-        }),
-        // Minify HTML and CSS.
-        minifyHTML({
-            include: ['./src/**/*.html'],
-            removeComments: production,
-            removeRedundantAttributes: true,
-            sortClassName: true,
-            collapseWhitespace: production
-        }),
-        // Import HTML and SVG as strings.
-        string({
-            include: ['./src/**/*.html', './src/**/*.svg']
-        }),
-        // Transpile TypeScript.
-        swc({
-            include: './src/**',
-            sourceMaps: true,
-            tsconfig: false,
-            env: {
-                targets: browserslist
-            },
-            jsc: {
-                loose: true,
-                externalHelpers: true,
-                parser: {
-                    syntax: 'typescript'
+const banner = `/*!
+ * THEOplayer Web UI v${version}
+ * License: ${license}
+ */`;
+
+export default defineConfig([
+    {
+        input: './src/index.ts',
+        output: [
+            {
+                file: `./dist/${fileName}.js`,
+                format: 'umd',
+                name: umdName,
+                sourcemap: true,
+                indent: false,
+                banner,
+                globals: {
+                    theoplayer: 'THEOplayer'
                 }
-            }
-        }),
-        // Transpile dependencies for older browsers.
-        swc({
-            include: './node_modules/**',
-            exclude: './src/**',
-            sourceMaps: true,
-            tsconfig: false,
-            env: {
-                targets: browserslist
             },
-            jsc: {
-                loose: true,
-                externalHelpers: true
+            {
+                file: `./dist/${fileName}.mjs`,
+                format: 'es',
+                sourcemap: true,
+                indent: false
             }
-        }),
-        ...(production
-            ? [
-                  minify({
-                      sourceMap: true,
-                      mangle: {
+        ],
+        context: 'self',
+        external: ['theoplayer'],
+        plugins: [
+            // Use TypeScript's module resolution for source files, and Node's for dependencies.
+            typescriptPaths(),
+            nodeResolve(),
+            // Run PostCSS on .css files.
+            postcss({
+                include: './src/**/*.css',
+                inject: false,
+                plugins: [
+                    postcssPresetEnv({
+                        browsers: browserslist,
+                        autoprefixer: { grid: 'no-autoplace' },
+                        enableClientSidePolyfills: false
+                    }),
+                    postcssMixins()
+                ],
+                minimize: production
+            }),
+            // Minify HTML and CSS.
+            minifyHTML({
+                include: ['./src/**/*.html'],
+                removeComments: production,
+                removeRedundantAttributes: true,
+                sortClassName: true,
+                collapseWhitespace: production
+            }),
+            // Import HTML and SVG as strings.
+            string({
+                include: ['./src/**/*.html', './src/**/*.svg']
+            }),
+            // Transpile TypeScript.
+            swc({
+                include: './src/**',
+                sourceMaps: true,
+                tsconfig: false,
+                env: {
+                    targets: browserslist
+                },
+                jsc: {
+                    loose: true,
+                    externalHelpers: true,
+                    parser: {
+                        syntax: 'typescript'
+                    }
+                }
+            }),
+            // Transpile dependencies for older browsers.
+            swc({
+                include: './node_modules/**',
+                exclude: './src/**',
+                sourceMaps: true,
+                tsconfig: false,
+                env: {
+                    targets: browserslist
+                },
+                jsc: {
+                    loose: true,
+                    externalHelpers: true
+                }
+            }),
+            ...(production
+                ? [
+                      minify({
+                          sourceMap: true,
+                          mangle: {
+                              toplevel: true
+                          },
                           toplevel: true
-                      },
-                      toplevel: true
-                  })
-              ]
-            : [])
-    ]
-});
+                      })
+                  ]
+                : [])
+        ]
+    },
+    {
+        input: './src/index.ts',
+        output: [
+            {
+                file: `./dist/${fileName}.d.ts`,
+                format: 'es',
+                indent: false,
+                banner,
+                footer: `export as namespace ${umdName};`
+            }
+        ],
+        external: ['theoplayer'],
+        plugins: [dts()]
+    }
+]);
