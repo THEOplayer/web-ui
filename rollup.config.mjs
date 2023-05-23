f4f083b36ba5af2f9864f0ea7549dcf72eeb9067
Mattias Buelens <mattias.buelens@theoplayer.com>
2023-05-23 17:02:59 +0200

    Fix indentation

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -24,77 +24,77 @@ const banner = `/*!
 const theoplayerModule = 'theoplayer/THEOplayer.chromeless';
 
 /**
-* @param {{configOutputDir?: string}} cliArgs
-* @return {import("rollup").RollupOptions[]}
-*/
+ * @param {{configOutputDir?: string}} cliArgs
+ * @return {import("rollup").RollupOptions[]}
+ */
 export default (cliArgs) => {
-const outputDir = cliArgs.configOutputDir || './dist';
-return defineConfig([
-    {
-        input: './src/index.ts',
-        output: [
-            {
-                file: path.join(outputDir, `${fileName}.js`),
-                format: 'umd',
-                name: umdName,
-                sourcemap: true,
-                indent: false,
-                banner,
-                globals: {
-                    [theoplayerModule]: 'THEOplayer'
+    const outputDir = cliArgs.configOutputDir || './dist';
+    return defineConfig([
+        {
+            input: './src/index.ts',
+            output: [
+                {
+                    file: path.join(outputDir, `${fileName}.js`),
+                    format: 'umd',
+                    name: umdName,
+                    sourcemap: true,
+                    indent: false,
+                    banner,
+                    globals: {
+                        [theoplayerModule]: 'THEOplayer'
+                    }
+                },
+                {
+                    file: path.join(outputDir, `${fileName}.mjs`),
+                    format: 'es',
+                    sourcemap: true,
+                    indent: false
                 }
-            },
-            {
-                file: path.join(outputDir, `${fileName}.mjs`),
-                format: 'es',
-                sourcemap: true,
-                indent: false
-            }
-        ],
-        context: 'self',
-        external: [theoplayerModule],
-        plugins: jsPlugins({ es5: false, production, sourcemap: true })
-    },
-    {
-        input: './src/index.ts',
-        output: [
-            {
-                file: path.join(outputDir, `${fileName}.es5.js`),
-                format: 'umd',
-                name: umdName,
-                sourcemap: false,
-                indent: false,
-                banner,
-                globals: {
-                    [theoplayerModule]: 'THEOplayer'
+            ],
+            context: 'self',
+            external: [theoplayerModule],
+            plugins: jsPlugins({ es5: false, production, sourcemap: true })
+        },
+        {
+            input: './src/index.ts',
+            output: [
+                {
+                    file: path.join(outputDir, `${fileName}.es5.js`),
+                    format: 'umd',
+                    name: umdName,
+                    sourcemap: false,
+                    indent: false,
+                    banner,
+                    globals: {
+                        [theoplayerModule]: 'THEOplayer'
+                    }
+                },
+                {
+                    file: path.join(outputDir, `${fileName}.es5.mjs`),
+                    format: 'es',
+                    sourcemap: false,
+                    indent: false
                 }
-            },
-            {
-                file: path.join(outputDir, `${fileName}.es5.mjs`),
-                format: 'es',
-                sourcemap: false,
-                indent: false
-            }
-        ],
-        context: 'self',
-        external: [theoplayerModule],
-        plugins: jsPlugins({ es5: true, production, sourcemap: false })
-    },
-    {
-        input: './src/index.ts',
-        output: [
-            {
-                file: path.join(outputDir, `${fileName}.d.ts`),
-                format: 'es',
-                indent: false,
-                banner,
-                footer: `export as namespace ${umdName};`
-            }
-        ],
-        external: [theoplayerModule],
-        plugins: [dts()]
-    }
-]);
+            ],
+            context: 'self',
+            external: [theoplayerModule],
+            plugins: jsPlugins({ es5: true, production, sourcemap: false })
+        },
+        {
+            input: './src/index.ts',
+            output: [
+                {
+                    file: path.join(outputDir, `${fileName}.d.ts`),
+                    format: 'es',
+                    indent: false,
+                    banner,
+                    footer: `export as namespace ${umdName};`
+                }
+            ],
+            external: [theoplayerModule],
+            plugins: [dts()]
+        }
+    ]);
 };
 
 /**
