d4fd5dfe289d12ae000fada8f5ce387bb76b0997
Jeroen Veltmans <jeroen.veltmans@theoplayer.com>
2023-10-11 14:48:20 +0200

    Add more back key codes

--- src/util/KeyCode.ts
+++ src/util/KeyCode.ts
@@ -8,16 +8,20 @@ export enum KeyCode {
     UP = 38,
     RIGHT = 39,
     DOWN = 40,
+    // Multiple back key configurations depending on the platform
+    // https://suite.st/docs/faq/tv-specific-keys-in-browser/
+    BACK_SAMSUNG = 88,
+    BACK_WEBOS = 461,
     BACK_TIZEN = 10009
 }
 
 export type ArrowKeyCode = KeyCode.LEFT | KeyCode.UP | KeyCode.RIGHT | KeyCode.DOWN;
-export type BackKeyCode = KeyCode.BACK_TIZEN | KeyCode.ESCAPE;
+export type BackKeyCode = KeyCode.BACK_TIZEN | KeyCode.ESCAPE | KeyCode.BACK_SAMSUNG | KeyCode.BACK_WEBOS;
 
 export function isArrowKey(keyCode: number): keyCode is ArrowKeyCode {
     return KeyCode.LEFT <= keyCode && keyCode <= KeyCode.DOWN;
 }
 
 export function isBackKey(keyCode: number): keyCode is BackKeyCode {
-    return keyCode === KeyCode.BACK_TIZEN || keyCode === KeyCode.ESCAPE;
+    return keyCode === KeyCode.BACK_TIZEN || keyCode === KeyCode.ESCAPE || keyCode === KeyCode.BACK_SAMSUNG || keyCode === KeyCode.BACK_WEBOS;
 }
