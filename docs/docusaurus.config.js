c1575aa2d1bd11cafaea03494a91d13c95c7f569
Mattias Buelens <mattias.buelens@theoplayer.com>
2023-11-23 16:13:22 +0100

    Run Prettier

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -4,110 +4,110 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'THEOplayer Open Video UI for Web',
-  tagline: 'UI component library for the THEOplayer Web SDK',
-  // favicon: 'img/favicon.ico',
+    title: 'THEOplayer Open Video UI for Web',
+    tagline: 'UI component library for the THEOplayer Web SDK',
+    // favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
-  url: 'https://theoplayer.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/web-ui/',
+    // Set the production url of your site here
+    url: 'https://theoplayer.github.io',
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: '/web-ui/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'THEOplayer', // Usually your GitHub org/user name.
-  projectName: 'web-ui', // Usually your repo name.
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: 'THEOplayer', // Usually your GitHub org/user name.
+    projectName: 'web-ui', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en']
-  },
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en']
+    },
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: './sidebars.js',
-          editUrl: 'https://github.com/THEOplayer/web-ui/tree/main/docs/'
-        },
-        theme: {
-          customCss: './src/css/custom.css'
-        }
-      })
-    ]
-  ],
+    presets: [
+        [
+            'classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: './sidebars.js',
+                    editUrl: 'https://github.com/THEOplayer/web-ui/tree/main/docs/'
+                },
+                theme: {
+                    customCss: './src/css/custom.css'
+                }
+            })
+        ]
+    ],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
-      navbar: {
-        title: 'THEOplayer Open Video UI for Web',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg'
-        },
-        items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial'
-          },
-          {
-            href: 'https://github.com/THEOplayer/web-ui',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro'
-              }
-            ]
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'GitHub',
-                href: 'https://github.com/THEOplayer/web-ui'
-              },
-              {
-                label: 'THEOplayer',
-                href: 'https://www.theoplayer.com'
-              }
-            ]
-          }
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} THEO Technologies. Built with Docusaurus.`
-      },
-      prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-      },
-    }),
+    themeConfig:
+        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            // Replace with your project's social card
+            image: 'img/docusaurus-social-card.jpg',
+            navbar: {
+                title: 'THEOplayer Open Video UI for Web',
+                logo: {
+                    alt: 'My Site Logo',
+                    src: 'img/logo.svg'
+                },
+                items: [
+                    {
+                        type: 'docSidebar',
+                        sidebarId: 'tutorialSidebar',
+                        position: 'left',
+                        label: 'Tutorial'
+                    },
+                    {
+                        href: 'https://github.com/THEOplayer/web-ui',
+                        label: 'GitHub',
+                        position: 'right'
+                    }
+                ]
+            },
+            footer: {
+                style: 'dark',
+                links: [
+                    {
+                        title: 'Docs',
+                        items: [
+                            {
+                                label: 'Tutorial',
+                                to: '/docs/intro'
+                            }
+                        ]
+                    },
+                    {
+                        title: 'More',
+                        items: [
+                            {
+                                label: 'GitHub',
+                                href: 'https://github.com/THEOplayer/web-ui'
+                            },
+                            {
+                                label: 'THEOplayer',
+                                href: 'https://www.theoplayer.com'
+                            }
+                        ]
+                    }
+                ],
+                copyright: `Copyright © ${new Date().getFullYear()} THEO Technologies. Built with Docusaurus.`
+            },
+            prism: {
+                theme: prismThemes.github,
+                darkTheme: prismThemes.dracula
+            }
+        })
 };
 
 export default config;
