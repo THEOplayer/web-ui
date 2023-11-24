a2b4c9d8a424d89fdfa9c06a0eb3991263c2e45a
Mattias Buelens <mattias.buelens@theoplayer.com>
2023-11-24 11:29:48 +0100

    Refactor all JSX to TSX

--- docs/src/components/HomepageFeatures/index.tsx
+++ docs/src/components/HomepageFeatures/index.tsx
@@ -1,8 +1,15 @@
+import React, { JSX } from 'react';
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 
-const FeatureList = [
+type FeatureItem = {
+    title: string;
+    Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+    description: JSX.Element;
+};
+
+const FeatureList: FeatureItem[] = [
     {
         title: 'Easy to Use',
         Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
@@ -27,7 +34,7 @@ const FeatureList = [
     }
 ];
 
-function Feature({ Svg, title, description }) {
+function Feature({ title, Svg, description }: FeatureItem) {
     return (
         <div className={clsx('col col--4')}>
             <div className="text--center">
@@ -41,7 +48,7 @@ function Feature({ Svg, title, description }) {
     );
 }
 
-export default function HomepageFeatures() {
+export default function HomepageFeatures(): JSX.Element {
     return (
         <section className={styles.features}>
             <div className="container">
--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,10 +1,11 @@
+import React, { JSX } from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
 import Heading from '@theme/Heading';
+
 import styles from './index.module.css';
 
 function HomepageHeader() {
@@ -26,7 +27,7 @@ function HomepageHeader() {
     );
 }
 
-export default function Home() {
+export default function Home(): JSX.Element {
     const { siteConfig } = useDocusaurusContext();
     return (
         <Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
