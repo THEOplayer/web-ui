7ad53d375550d3ba70e5bb26ddebe5fda3d62aae
Mattias Buelens <mattias.buelens@theoplayer.com>
2024-01-31 15:54:36 +0100

    Add heading slot to React menus

--- react/src/components/PlaybackRateMenu.tsx
+++ react/src/components/PlaybackRateMenu.tsx
@@ -1,10 +1,28 @@
-import { createComponent } from '@lit/react';
+import { createComponent, type WebComponentProps } from '@lit/react';
 import { PlaybackRateMenu as PlaybackRateMenuElement } from '@theoplayer/web-ui';
+import type { PropsWithoutRef, ReactNode } from 'react';
 import * as React from 'react';
+import { Slotted } from '../slotted';
 
-export const PlaybackRateMenu = createComponent({
+const RawPlaybackRateMenu = createComponent({
     tagName: 'theoplayer-playback-rate-menu',
     displayName: 'PlaybackRateMenu',
     elementClass: PlaybackRateMenuElement,
     react: React
 });
+
+export interface PlaybackRateMenuProps extends PropsWithoutRef<WebComponentProps<PlaybackRateMenuElement>> {
+    /**
+     * A slot for the menu's heading.
+     */
+    heading?: ReactNode;
+}
+
+export const PlaybackRateMenu = ({ heading, children, ...props }: PlaybackRateMenuProps) => {
+    return (
+        <RawPlaybackRateMenu {...props}>
+            <Slotted slot="heading">{heading}</Slotted>
+            {children}
+        </RawPlaybackRateMenu>
+    );
+};
--- react/src/components/SettingsMenu.tsx
+++ react/src/components/SettingsMenu.tsx
@@ -1,10 +1,28 @@
-import { createComponent } from '@lit/react';
+import { createComponent, type WebComponentProps } from '@lit/react';
 import { SettingsMenu as SettingsMenuElement } from '@theoplayer/web-ui';
+import type { PropsWithoutRef, ReactNode } from 'react';
 import * as React from 'react';
+import { Slotted } from '../slotted';
 
-export const SettingsMenu = createComponent({
+const RawSettingsMenu = createComponent({
     tagName: 'theoplayer-settings-menu',
     displayName: 'SettingsMenu',
     elementClass: SettingsMenuElement,
     react: React
 });
+
+export interface SettingsMenuProps extends PropsWithoutRef<WebComponentProps<SettingsMenuElement>> {
+    /**
+     * A slot for the menu's heading.
+     */
+    heading?: ReactNode;
+}
+
+export const SettingsMenu = ({ heading, children, ...props }: SettingsMenuProps) => {
+    return (
+        <RawSettingsMenu {...props}>
+            <Slotted slot="heading">{heading}</Slotted>
+            {children}
+        </RawSettingsMenu>
+    );
+};
