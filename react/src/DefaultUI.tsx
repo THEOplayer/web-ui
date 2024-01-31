82f25354336cf4b8238e1ff35f43910acfda7e1c
Mattias Buelens <mattias.buelens@theoplayer.com>
2024-01-31 14:02:39 +0100

    Add slots to React DefaultUI

--- react/src/DefaultUI.tsx
+++ react/src/DefaultUI.tsx
@@ -5,6 +5,7 @@ import type { ChromelessPlayer } from 'theoplayer/chromeless';
 import { createComponent, type WebComponentProps } from '@lit/react';
 import { usePlayer } from './util';
 import { PlayerContext } from './context';
+import type { SlottedChildren } from './slotted';
 
 const RawDefaultUI = createComponent({
     tagName: 'theoplayer-default-ui',
@@ -16,15 +17,49 @@ const RawDefaultUI = createComponent({
     } as const
 });
 
-export interface DefaultUIProps extends PropsWithoutRef<WebComponentProps<DefaultUIElement>> {
+export interface DefaultUIProps extends PropsWithoutRef<Omit<WebComponentProps<DefaultUIElement>, 'title'>> {
+    /**
+     * A slot for the stream's title in the top control bar.
+     */
+    title?: SlottedChildren;
+    /**
+     * A slot for extra UI controls in the top control bar.
+     */
+    topControlBar?: SlottedChildren;
+    /**
+     * A slot for extra UI controls in the bottom control bar.
+     */
+    bottomControlBar?: SlottedChildren;
+    /**
+     * A slot for extra menus (see {@link Menu}).
+     */
+    menu?: SlottedChildren;
+    /**
+     * Use a named slot instead, such as:
+     *  - {@link title}
+     *  - {@link topControlBar}
+     *  - {@link bottomControlBar}
+     *  - {@link menu}
+     */
+    children?: never;
+    /**
+     * Called when the backing player is created.
+     *
+     * @param player
+     */
     onReady?: (player: ChromelessPlayer) => void;
 }
 
-export const DefaultUI = ({ children, onReady, ...props }: DefaultUIProps) => {
+export const DefaultUI = ({ title, topControlBar, bottomControlBar, menu, onReady, ...props }: DefaultUIProps) => {
     const { player, setUi, onReadyHandler } = usePlayer(onReady);
     return (
         <RawDefaultUI {...props} ref={setUi} onReady={onReadyHandler}>
-            <PlayerContext.Provider value={player}>{children}</PlayerContext.Provider>
+            <PlayerContext.Provider value={player}>
+                {title?.({ slot: 'title' })}
+                {topControlBar?.({ slot: 'top-control-bar' })}
+                {bottomControlBar?.({ slot: 'bottom-control-bar' })}
+                {menu?.({ slot: 'menu' })}
+            </PlayerContext.Provider>
         </RawDefaultUI>
     );
 };
