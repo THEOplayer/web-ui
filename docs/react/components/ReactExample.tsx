1a5db4fc4e4316998aaba3db4562fd26d16e23c5
Mattias Buelens <mattias.buelens@theoplayer.com>
2024-07-30 12:14:13 +0200

    Replace <ReactExample> with <Example>

--- docs/react/components/ReactExample.tsx
+++
@@ -1,8 +0,0 @@
-import React, { type JSX } from 'react';
-import Example, { type Props as ExampleProps } from '../../components/Example';
-
-export interface Props extends ExampleProps {}
-
-export default function ReactExample(props: Props): JSX.Element {
-    return <Example {...props} />;
-}
