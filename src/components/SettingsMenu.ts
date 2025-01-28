5be8af13d977e4a228054ab8c483ea04e9fafd92
Mattias Buelens <mattias.buelens@theoplayer.com>
2025-01-28 16:55:48 +0100

    Rework MenuGroup to use render() method

--- src/components/SettingsMenu.ts
+++ src/components/SettingsMenu.ts
@@ -1,13 +1,11 @@
-import { MenuGroup, menuGroupTemplate } from './MenuGroup';
-import settingsMenuHtml from './SettingsMenu.html';
+import { html, type TemplateResult } from 'lit-html';
+import { MenuGroup } from './MenuGroup';
 import menuTableCss from './MenuTable.css';
 
 // Load components used in template
 import './ActiveQualityDisplay';
 import './PlaybackRateDisplay';
 import './PlaybackRateMenu';
-
-const template = menuGroupTemplate(settingsMenuHtml, menuTableCss);
 
 /**
  * `<theoplayer-settings-menu>` - A menu to change the settings of the player,
@@ -18,8 +16,40 @@ const template = menuGroupTemplate(settingsMenuHtml, menuTableCss);
  * @group Components
  */
 export class SettingsMenu extends MenuGroup {
-    constructor() {
-        super({ template });
+    protected override render(): TemplateResult {
+        return this.renderMenuGroup(
+            html`
+                <theoplayer-menu>
+                    <span slot="heading"><slot name="heading">Settings</slot></span>
+                    <table class="theoplayer-menu-table">
+                        <tr>
+                            <td><span>Quality</span></td>
+                            <td>
+                                <theoplayer-menu-button menu="quality-menu">
+                                    <theoplayer-active-quality-display></theoplayer-active-quality-display>
+                                </theoplayer-menu-button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><span>Playback speed</span></td>
+                            <td>
+                                <theoplayer-menu-button menu="playback-rate-menu">
+                                    <theoplayer-playback-rate-display></theoplayer-playback-rate-display>
+                                </theoplayer-menu-button>
+                            </td>
+                        </tr>
+                    </table>
+                </theoplayer-menu>
+                <theoplayer-menu id="quality-menu" menu-close-on-input hidden>
+                    <span slot="heading">Quality</span>
+                    <theoplayer-quality-radio-group></theoplayer-quality-radio-group>
+                </theoplayer-menu>
+                <theoplayer-playback-rate-menu id="playback-rate-menu" menu-close-on-input hidden>
+                    <span slot="heading">Playback speed</span>
+                </theoplayer-playback-rate-menu>
+            `,
+            menuTableCss
+        );
     }
 }
 
