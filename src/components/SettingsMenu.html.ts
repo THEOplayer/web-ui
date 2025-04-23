92fb87b870d9f31224f42aa6a05ae3a0ccc428b9
Mattias Buelens <mattias.buelens@theoplayer.com>
2025-04-23 18:09:17 +0200

    Run Prettier

--- src/components/SettingsMenu.html.ts
+++ src/components/SettingsMenu.html.ts
@@ -1,24 +1,24 @@
 import { html } from 'lit-html';
 
 export default html`
-<theoplayer-menu>
-    <span slot="heading"><slot name="heading">Settings</slot></span>
-    <div class="theoplayer-menu-table">
-        <span>Quality</span>
-        <theoplayer-menu-button menu="quality-menu">
-            <theoplayer-active-quality-display></theoplayer-active-quality-display>
-        </theoplayer-menu-button>
-        <span>Playback speed</span>
-        <theoplayer-menu-button menu="playback-rate-menu">
-            <theoplayer-playback-rate-display></theoplayer-playback-rate-display>
-        </theoplayer-menu-button>
-    </div>
-</theoplayer-menu>
-<theoplayer-menu id="quality-menu" menu-close-on-input hidden>
-    <span slot="heading">Quality</span>
-    <theoplayer-quality-radio-group></theoplayer-quality-radio-group>
-</theoplayer-menu>
-<theoplayer-playback-rate-menu id="playback-rate-menu" menu-close-on-input hidden>
-    <span slot="heading">Playback speed</span>
-</theoplayer-playback-rate-menu>
+    <theoplayer-menu>
+        <span slot="heading"><slot name="heading">Settings</slot></span>
+        <div class="theoplayer-menu-table">
+            <span>Quality</span>
+            <theoplayer-menu-button menu="quality-menu">
+                <theoplayer-active-quality-display></theoplayer-active-quality-display>
+            </theoplayer-menu-button>
+            <span>Playback speed</span>
+            <theoplayer-menu-button menu="playback-rate-menu">
+                <theoplayer-playback-rate-display></theoplayer-playback-rate-display>
+            </theoplayer-menu-button>
+        </div>
+    </theoplayer-menu>
+    <theoplayer-menu id="quality-menu" menu-close-on-input hidden>
+        <span slot="heading">Quality</span>
+        <theoplayer-quality-radio-group></theoplayer-quality-radio-group>
+    </theoplayer-menu>
+    <theoplayer-playback-rate-menu id="playback-rate-menu" menu-close-on-input hidden>
+        <span slot="heading">Playback speed</span>
+    </theoplayer-playback-rate-menu>
 `;
