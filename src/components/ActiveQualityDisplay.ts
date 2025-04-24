06b27a22f03bca23f51a25f50c0a099b09f3c205
Mattias Buelens <mattias.buelens@theoplayer.com>
2025-04-24 16:35:22 +0200

    Rework ActiveQualityDisplay

--- src/components/ActiveQualityDisplay.ts
+++ src/components/ActiveQualityDisplay.ts
@@ -1,60 +1,27 @@
-import { StateReceiverMixin } from './StateReceiverMixin';
-import { setTextContent } from '../util/CommonUtils';
+import { html, type HTMLTemplateResult, LitElement } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import { stateReceiver } from './StateReceiverMixin';
 import type { VideoQuality } from 'theoplayer/chromeless';
 import { formatQualityLabel } from '../util/TrackUtils';
-import * as shadyCss from '@webcomponents/shadycss';
 
 /**
  * `<theoplayer-active-quality-display>` - A control that displays the name of the active video quality.
  *
  * @group Components
  */
-export class ActiveQualityDisplay extends StateReceiverMixin(HTMLElement, ['activeVideoQuality', 'targetVideoQualities']) {
-    private readonly _spanEl: HTMLSpanElement;
+@customElement('theoplayer-active-quality-display')
+@stateReceiver(['activeVideoQuality', 'targetVideoQualities'])
+export class ActiveQualityDisplay extends LitElement {
     private _activeVideoQuality: VideoQuality | undefined = undefined;
     private _targetVideoQualities: VideoQuality[] | undefined = undefined;
 
-    constructor() {
-        super();
-        const shadowRoot = this.attachShadow({ mode: 'open' });
-        this._spanEl = document.createElement('span');
-        shadowRoot.appendChild(this._spanEl);
+    @property({ reflect: false, attribute: false })
+    accessor activeVideoQuality: VideoQuality | undefined = undefined;
 
-        this._upgradeProperty('activeVideoQuality');
-        this._upgradeProperty('targetVideoQualities');
-    }
+    @property({ reflect: false, attribute: false })
+    accessor targetVideoQualities: VideoQuality[] | undefined = undefined;
 
-    connectedCallback(): void {
-        shadyCss.styleElement(this);
-    }
-
-    protected _upgradeProperty(prop: keyof this) {
-        if (this.hasOwnProperty(prop)) {
-            let value = this[prop];
-            delete this[prop];
-            this[prop] = value;
-        }
-    }
-
-    get activeVideoQuality(): VideoQuality | undefined {
-        return this._activeVideoQuality;
-    }
-
-    set activeVideoQuality(quality: VideoQuality | undefined) {
-        this._activeVideoQuality = quality;
-        this.update_();
-    }
-
-    get targetVideoQualities(): VideoQuality[] | undefined {
-        return this._targetVideoQualities;
-    }
-
-    set targetVideoQualities(qualities: VideoQuality[] | undefined) {
-        this._targetVideoQualities = qualities;
-        this.update_();
-    }
-
-    private update_(): void {
+    protected override render(): HTMLTemplateResult {
         // If no target quality is selected, or more than one target quality is selected,
         // treat as "automatic" quality selection.
         const hasSingleTargetQuality = this._targetVideoQualities !== undefined && this._targetVideoQualities.length === 1;
@@ -70,11 +37,9 @@ export class ActiveQualityDisplay extends StateReceiverMixin(HTMLElement, ['acti
             // Automatic quality selection: "Automatic" or "Automatic (720p)"
             label = `Automatic${qualityLabel ? ` (${qualityLabel})` : ''}`;
         }
-        setTextContent(this._spanEl, label);
+        return html`<span>${label}</span>`;
     }
 }
-
-customElements.define('theoplayer-active-quality-display', ActiveQualityDisplay);
 
 declare global {
     interface HTMLElementTagNameMap {
