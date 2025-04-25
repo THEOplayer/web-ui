82845ac111fcc4ad1e4ea46d77bf8c824f215df2
Mattias Buelens <mattias.buelens@theoplayer.com>
2025-04-25 14:24:21 +0200

    Rework DurationDisplay and PlaybackRateDisplay

--- src/components/PlaybackRateDisplay.ts
+++ src/components/PlaybackRateDisplay.ts
@@ -1,48 +1,25 @@
-import { StateReceiverMixin } from './StateReceiverMixin';
-import { setTextContent } from '../util/CommonUtils';
-import * as shadyCss from '@webcomponents/shadycss';
+import { html, type HTMLTemplateResult, LitElement } from 'lit';
+import { stateReceiver } from './StateReceiverMixin';
+import { customElement, state } from 'lit/decorators.js';
 
 /**
  * `<theoplayer-playback-rate-display>` - A control that displays the current playback rate of the player.
  *
  * @group Components
  */
-export class PlaybackRateDisplay extends StateReceiverMixin(HTMLElement, ['playbackRate']) {
-    private readonly _spanEl: HTMLSpanElement;
-    private _playbackRate: number = 1;
+@customElement('theoplayer-playback-rate-display')
+@stateReceiver(['playbackRate'])
+export class PlaybackRateDisplay extends LitElement {
+    /**
+     * The current playback rate.
+     */
+    @state()
+    accessor playbackRate: number = 1;
 
-    constructor() {
-        super();
-        const shadowRoot = this.attachShadow({ mode: 'open' });
-        this._spanEl = document.createElement('span');
-        shadowRoot.appendChild(this._spanEl);
-
-        this._upgradeProperty('playbackRate');
-    }
-
-    connectedCallback(): void {
-        shadyCss.styleElement(this);
-    }
-
-    protected _upgradeProperty(prop: keyof this) {
-        if (this.hasOwnProperty(prop)) {
-            let value = this[prop];
-            delete this[prop];
-            this[prop] = value;
-        }
-    }
-
-    get playbackRate(): number {
-        return this._playbackRate;
-    }
-
-    set playbackRate(value: number) {
-        this._playbackRate = value;
-        setTextContent(this._spanEl, value === 1 ? 'Normal' : `${value}x`);
+    protected override render(): HTMLTemplateResult {
+        return html`<span>${this.playbackRate === 1 ? 'Normal' : `${this.playbackRate}x`}</span>`;
     }
 }
-
-customElements.define('theoplayer-playback-rate-display', PlaybackRateDisplay);
 
 declare global {
     interface HTMLElementTagNameMap {
