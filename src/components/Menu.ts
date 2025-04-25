94e91958665f96c8b73efe0989b88c1648d3c79a
Mattias Buelens <mattias.buelens@theoplayer.com>
2025-04-25 11:48:39 +0200

    Rework menus

--- src/components/Menu.ts
+++ src/components/Menu.ts
@@ -1,25 +1,11 @@
-import * as shadyCss from '@webcomponents/shadycss';
+import { html, type HTMLTemplateResult, LitElement } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
 import menuCss from './Menu.css';
 import { CLOSE_MENU_EVENT, type CloseMenuEvent } from '../events/CloseMenuEvent';
 import { MENU_CHANGE_EVENT, type MenuChangeEvent } from '../events/MenuChangeEvent';
 import { createCustomEvent } from '../util/EventUtils';
 import { Attribute } from '../util/Attribute';
 import { toggleAttribute } from '../util/CommonUtils';
-import { createTemplate } from '../util/TemplateUtils';
-
-export interface MenuOptions {
-    template?: HTMLTemplateElement;
-}
-
-export function menuTemplate(heading: string, content: string, extraCss: string = ''): string {
-    return (
-        `<style>${menuCss}${extraCss}</style>` +
-        `<div part="heading"><theoplayer-menu-close-button></theoplayer-menu-close-button>${heading}</div>` +
-        `<div part="content">${content}</div>`
-    );
-}
-
-const defaultTemplate = createTemplate('theoplayer-menu', menuTemplate('<slot name="heading"></slot>', '<slot></slot>'));
 
 /**
  * `<theoplayer-menu>` - A menu that can be opened on top of the player.
@@ -34,96 +20,71 @@ const defaultTemplate = createTemplate('theoplayer-menu', menuTemplate('<slot na
  *
  * @group Components
  */
-export class Menu extends HTMLElement {
-    static get observedAttributes() {
-        return [Attribute.MENU_OPENED, Attribute.MENU_CLOSE_ON_INPUT];
-    }
+@customElement('theoplayer-menu')
+export class Menu extends LitElement {
+    static override styles = [menuCss];
+    static override shadowRootOptions = {
+        ...LitElement.shadowRootOptions,
+        delegatesFocus: true
+    };
 
-    private readonly _contentEl: HTMLElement;
+    private _menuOpened: boolean = false;
 
     /**
      * Creates a menu.
      *
      * By default, the button has an unnamed `<slot>` for its contents, and a named `"heading"` `<slot>` for its heading text.
-     * Subclasses can override this by passing a different {@link MenuOptions.template} in the options,
-     * using {@link menuTemplate} to correctly style the custom template.
-     *
-     * @param options - The options for this menu.
+     * Subclasses can override this by overriding {@link renderMenuHeading} and/or {@link renderMenuContent}.
      */
-    constructor(options?: MenuOptions) {
+    constructor() {
         super();
-        const template = options?.template ?? defaultTemplate();
-        const shadowRoot = this.attachShadow({ mode: 'open', delegatesFocus: true });
-        shadowRoot.appendChild(template.content.cloneNode(true));
-
-        this._contentEl = shadowRoot.querySelector('[part="content"]')!;
-    }
-
-    protected _upgradeProperty(prop: keyof this) {
-        if (this.hasOwnProperty(prop)) {
-            let value = this[prop];
-            delete this[prop];
-            this[prop] = value;
-        }
     }
 
     connectedCallback(): void {
-        shadyCss.styleElement(this);
+        super.connectedCallback();
 
-        if (!this.hasAttribute(Attribute.MENU_OPENED)) {
-            this.setAttribute('hidden', '');
+        if (!this.menuOpened_) {
+            this.setAttribute(Attribute.HIDDEN, '');
         }
-
-        this._contentEl.addEventListener('input', this._onContentInput);
-    }
-
-    disconnectedCallback(): void {
-        this._contentEl.removeEventListener('input', this._onContentInput);
     }
 
     /**
      * Whether to automatically close the menu whenever one of its controls
      * receives an input (e.g. when a radio button is clicked).
      */
-    get closeOnInput(): boolean {
-        return this.hasAttribute(Attribute.MENU_CLOSE_ON_INPUT);
+    @property({ reflect: true, type: Boolean, attribute: Attribute.MENU_CLOSE_ON_INPUT })
+    accessor closeOnInput: boolean = false;
+
+    private get menuOpened_(): boolean {
+        return this._menuOpened;
     }
 
-    set closeOnInput(value: boolean) {
-        toggleAttribute(this, Attribute.MENU_CLOSE_ON_INPUT, value);
+    @property({ reflect: true, state: true, type: Boolean, attribute: Attribute.MENU_OPENED })
+    private set menuOpened_(menuOpened: boolean) {
+        if (this._menuOpened === menuOpened) return;
+        this._menuOpened = menuOpened;
+        toggleAttribute(this, Attribute.HIDDEN, !menuOpened);
+        const changeEvent: MenuChangeEvent = createCustomEvent(MENU_CHANGE_EVENT, { bubbles: true });
+        this.dispatchEvent(changeEvent);
     }
 
     /**
      * Open the menu.
      */
     openMenu(): void {
-        this.setAttribute(Attribute.MENU_OPENED, '');
+        this.menuOpened_ = true;
     }
 
     /**
      * Close the menu.
      */
     closeMenu(): void {
-        this.removeAttribute(Attribute.MENU_OPENED);
-    }
-
-    attributeChangedCallback(attrName: string, oldValue: any, newValue: any) {
-        if (newValue === oldValue) {
-            return;
-        }
-        if (attrName === Attribute.MENU_OPENED) {
-            toggleAttribute(this, Attribute.HIDDEN, newValue == null);
-            const changeEvent: MenuChangeEvent = createCustomEvent(MENU_CHANGE_EVENT, { bubbles: true });
-            this.dispatchEvent(changeEvent);
-        }
-        if (Menu.observedAttributes.indexOf(attrName as Attribute) >= 0) {
-            shadyCss.styleSubtree(this);
-        }
+        this.menuOpened_ = false;
     }
 
     private readonly _onContentInput = (): void => {
         // Close menu when clicking any button
-        if (this.hasAttribute(Attribute.MENU_CLOSE_ON_INPUT)) {
+        if (this.closeOnInput) {
             const event: CloseMenuEvent = createCustomEvent(CLOSE_MENU_EVENT, {
                 bubbles: true,
                 composed: true
@@ -131,9 +92,23 @@ export class Menu extends HTMLElement {
             this.dispatchEvent(event);
         }
     };
-}
 
-customElements.define('theoplayer-menu', Menu);
+    protected override render(): HTMLTemplateResult {
+        return html`<div part="heading">
+                <theoplayer-menu-close-button></theoplayer-menu-close-button>
+                ${this.renderMenuHeading()}
+            </div>
+            <div part="content" @input=${this._onContentInput}>${this.renderMenuContent()}</div>`;
+    }
+
+    protected renderMenuHeading(): HTMLTemplateResult {
+        return html`<slot name="heading"></slot>`;
+    }
+
+    protected renderMenuContent(): HTMLTemplateResult {
+        return html`<slot></slot>`;
+    }
+}
 
 declare global {
     interface HTMLElementTagNameMap {
