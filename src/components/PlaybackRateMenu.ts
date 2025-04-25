4c8e72760cd0262fa7cc42c773ba47a2aae7da7b
Mattias Buelens <mattias.buelens@theoplayer.com>
2025-04-25 20:12:07 +0200

    Simplify PlaybackRateMenu

--- src/components/PlaybackRateMenu.ts
+++ src/components/PlaybackRateMenu.ts
@@ -1,10 +1,11 @@
 import { html, type HTMLTemplateResult } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { Menu } from './Menu';
-import playbackRateMenuHtml from './PlaybackRateMenu.html';
 
 // Load components used in template
 import './PlaybackRateRadioGroup';
+
+const PLAYBACK_RATES = [0.25, 0.5, 1, 1.25, 1.5, 2];
 
 /**
  * `<theoplayer-playback-rate-menu>` - A menu to change the playback rate of the player.
@@ -20,7 +21,11 @@ export class PlaybackRateMenu extends Menu {
     }
 
     protected override renderMenuContent(): HTMLTemplateResult {
-        return playbackRateMenuHtml;
+        return html`<theoplayer-playback-rate-radio-group>
+            ${PLAYBACK_RATES.map(
+                (rate) => html`<theoplayer-radio-button value=${rate}>${rate === 1 ? 'Normal' : `${rate}x`}</theoplayer-radio-button>`
+            )}
+        </theoplayer-playback-rate-radio-group>`;
     }
 }
 
