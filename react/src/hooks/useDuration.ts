572e8a2cba4d50dbc53a9b4a0f36b95fffdedd8e
Mattias Buelens <mattias.buelens@theoplayer.com>
2025-04-28 14:21:17 +0200

    Fix imports

--- react/src/hooks/useDuration.ts
+++ react/src/hooks/useDuration.ts
@@ -1,6 +1,6 @@
 import { useCallback, useContext, useSyncExternalStore } from 'react';
 import { PlayerContext } from '../context';
-import type { PlayerEventMap } from 'theoplayer';
+import type { PlayerEventMap } from 'theoplayer/chromeless';
 
 const DURATION_CHANGE_EVENTS = ['durationchange', 'emptied'] satisfies ReadonlyArray<keyof PlayerEventMap>;
 
