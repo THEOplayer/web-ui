d7296fd493d193cecc795444baeb9ba8245759fc
Mattias Buelens <mattias.buelens@theoplayer.com>
2025-05-06 18:21:19 +0200

    Fix typo

--- src/DefaultUI.ts
+++ src/DefaultUI.ts
@@ -163,7 +163,7 @@ export class DefaultUI extends LitElement {
     }
 
     /**
-     * Whether to automatically adjusts the player's height to fit the video's aspect ratio.
+     * Whether to automatically adjust the player's height to fit the video's aspect ratio.
      */
     @property({ reflect: true, type: Boolean, attribute: Attribute.FLUID })
     accessor fluid: boolean = false;
