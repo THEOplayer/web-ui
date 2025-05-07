eae8eb5387a78cde7c79884603b8ececc3ee7131
Mattias Buelens <mattias.buelens@theoplayer.com>
2025-05-07 15:44:32 +0200

    Load polyfill-support from Lit itself

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,3 +1,3 @@
 import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js';
 import '@webcomponents/webcomponentsjs/webcomponents-bundle.js';
-import 'lit-html/polyfill-support.js';
+import 'lit/polyfill-support.js';
