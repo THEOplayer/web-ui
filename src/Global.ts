4f3bdd1c1f092be06c347dd36ff000b566ea7ac3
Mattias Buelens <mattias.buelens@theoplayer.com>
2025-09-24 18:02:57 +0200

    Use `adoptedStylesheets` for global CSS

--- src/Global.ts
+++ src/Global.ts
@@ -1,5 +1,6 @@
 import globalCss from './Global.css';
 import { setTextContent } from './util/CommonUtils';
+import { supportsAdoptingStyleSheets } from 'lit';
 
 const GLOBAL_STYLE_ID = 'theoplayer-ui-global-styles';
 
@@ -9,13 +10,18 @@ export function addGlobalStyles() {
     if (globalStylesAdded) {
         return;
     }
-    if (document.getElementById(GLOBAL_STYLE_ID)) {
-        globalStylesAdded = true;
-        return;
+    if (supportsAdoptingStyleSheets) {
+        document.adoptedStyleSheets.push(globalCss.styleSheet!);
+    } else if (!document.getElementById(GLOBAL_STYLE_ID)) {
+        const styleEl = document.createElement('style');
+        styleEl.id = GLOBAL_STYLE_ID;
+        // See https://lit.dev/docs/api/LitElement/#LitElement.styles
+        const nonce = (window as any)['litNonce'];
+        if (nonce !== undefined) {
+            styleEl.setAttribute('nonce', nonce);
+        }
+        setTextContent(styleEl, globalCss.cssText);
+        document.head.appendChild(styleEl);
     }
-    const styleEl = document.createElement('style');
-    styleEl.id = GLOBAL_STYLE_ID;
-    setTextContent(styleEl, globalCss);
-    document.head.appendChild(styleEl);
     globalStylesAdded = true;
 }
