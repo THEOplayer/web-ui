a005121cae8618068ce70c304a1b1b502b09695e
Mattias Buelens <mattias.buelens@theoplayer.com>
2025-09-25 12:45:23 +0200

    Resolve `html` and `css` links from Lit docs

---
+++ scripts/typedoc-symbol-resolver.mjs
@@ -0,0 +1,20 @@
+/**
+ * @param {import('typedoc').Application} app
+ */
+export function load(app) {
+    app.converter.addUnknownSymbolResolver((declaration, _refl, _part, _symbolId) => {
+        // The Lit docs have some references to `html` and `css` without importing these types from `lit-element`.
+        // Resolve them manually.
+        if (declaration.moduleSource === undefined) {
+            const path = declaration.symbolReference?.path;
+            if (path && path.length === 1) {
+                switch (path[0].path) {
+                    case 'css':
+                        return 'https://lit.dev/docs/api/styles/#css';
+                    case 'html':
+                        return 'https://lit.dev/docs/api/templates/#html';
+                }
+            }
+        }
+    });
+}
