e849a87afc2f0f3f45ded64b00abcbefae49f8e1
Mattias Buelens <mattias.buelens@theoplayer.com>
2025-09-25 11:57:40 +0200

    Link Lit types to their API docs

--- typedoc.config.mjs
+++ typedoc.config.mjs
@@ -12,10 +12,26 @@ export default {
     excludePrivate: true,
     excludeInternal: true,
     excludeExternals: true,
+    excludeTags: [
+        '@nocollapse' // appears a lot in Lit docs
+    ],
     externalDocumentation: {
         theoplayer: {
             dtsPath: '~/THEOplayer.d.ts',
             externalBaseURL: 'https://www.theoplayer.com/docs/theoplayer/v8/api-reference/web'
+        }
+    },
+    externalSymbolLinkMappings: {
+        'lit-element': {
+            LitElement: 'https://lit.dev/docs/api/LitElement/',
+            '*': 'https://lit.dev/docs/'
+        },
+        '@lit/reactive-element': {
+            html: 'https://lit.dev/docs/api/templates/#html',
+            TemplateResult: 'https://lit.dev/docs/api/templates/#TemplateResult',
+            css: 'https://lit.dev/docs/api/styles/#css',
+            CSSResult: 'https://lit.dev/docs/api/styles/#CSSResult',
+            '*': 'https://lit.dev/docs/'
         }
     }
 };
