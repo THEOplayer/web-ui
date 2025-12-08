b19958fc1d535d7dd1019b081839f7e28f269bc4
Wonne Joosen <wonne.joosen@theoplayer.com>
2025-12-08 20:39:27 +0100

    don't toggle when seeking or paused

--- src/components/Range.ts
+++ src/components/Range.ts
@@ -169,7 +169,7 @@ export abstract class Range extends StateReceiverMixin(HTMLElement, ['deviceType
         const hasValue = newValue != null;
         if (attrName === Attribute.DISABLED || attrName === Attribute.INERT) {
             this.setAttribute('aria-disabled', this.disabled ? 'true' : 'false');
-            toggleAttribute(this._rangeEl, Attribute.DISABLED, this.disabled || this.inert);
+            toggleAttribute(this._rangeEl, Attribute.DISABLED, (this.disabled || this.inert) && !this.player?.paused && !this.player?.seeking);
         } else if (attrName === Attribute.HIDDEN) {
             if (!hasValue) {
                 this.update();
