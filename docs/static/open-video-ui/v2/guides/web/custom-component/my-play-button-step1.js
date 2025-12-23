f1cae482b3d6aef7f921f4be273538098ae78384
Mattias Buelens <mattias.buelens@theoplayer.com>
2025-12-23 12:09:34 +0100

    Rework custom play button guide

--- docs/static/open-video-ui/v2/guides/web/custom-component/my-play-button-step1.js
+++ docs/static/open-video-ui/v2/guides/web/custom-component/my-play-button-step1.js
@@ -1,17 +1,14 @@
-import { Button, buttonTemplate } from '@theoplayer/web-ui';
-
-// This <template> contains the content of our custom button.
-// Its contents will be cloned into every <my-play-button>.
-const template = document.createElement('template');
-template.innerHTML = buttonTemplate('Play');
+import { Button } from '@theoplayer/web-ui';
+import { html } from 'lit';
 
 // Define a new class for our custom element.
 // We extend the `Button` class from Open Video UI for Web,
 // so we can inherit some logic and styles.
 export class MyPlayButton extends Button {
-    constructor() {
-        // Pass our custom template to the super constructor.
-        super({ template });
+    // This method is called whenever the button is (re-)rendered.
+    // See: https://lit.dev/docs/components/rendering/
+    render() {
+        return html`Play`;
     }
 
     // This method is called whenever the button gets clicked.
@@ -22,4 +19,7 @@ export class MyPlayButton extends Button {
 
 // Define our class as the constructor for our custom element.
 // This allows us to use <my-play-button> anywhere in our HTML.
+// If you're using TypeScript, you can also use
+// Lit's @customElement decorator on the MyPlayButton class.
+// See: https://lit.dev/docs/components/defining/
 customElements.define('my-play-button', MyPlayButton);
